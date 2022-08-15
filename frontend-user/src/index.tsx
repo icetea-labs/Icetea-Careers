2005c9112a3bd3a99a7f48aa3de84b0c1e1ee497
Ha Nguyen <ha.nguyen@icetea.io>
2022-08-15 16:02:01 +0700

    using hash router

--- frontend-user/src/index.tsx
+++ frontend-user/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import "./index.css";
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
