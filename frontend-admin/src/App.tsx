f86ff1d6d7f4acfeecfd3b10a2f8540211b23228
Ha Nguyen <38517373+theforec@users.noreply.github.com>
2022-08-26 15:10:39 +0700

    FE admin: config axios

--- frontend-admin/src/App.tsx
+++ frontend-admin/src/App.tsx
@@ -4,30 +4,15 @@ import "./App.css";
 import createRoutes from "./routes";
 
 declare module "@mui/material/styles" {
-  interface TypographyVariants {
-    nn42032: React.CSSProperties;
-    nn62032: React.CSSProperties;
-    nn72032: React.CSSProperties;
-    nn74048: React.CSSProperties;
-  }
+  interface TypographyVariants {}
 
   // allow configuration using `createTheme`
-  interface TypographyVariantsOptions {
-    nn42032?: React.CSSProperties;
-    nn62032?: React.CSSProperties;
-    nn72032?: React.CSSProperties;
-    nn74048?: React.CSSProperties;
-  }
+  interface TypographyVariantsOptions {}
 }
 
 // Update the Typography's variant prop options
 declare module "@mui/material/Typography" {
-  interface TypographyPropsVariantOverrides {
-    nn42032: true;
-    nn62032: true;
-    nn72032: true;
-    nn74048: true;
-  }
+  interface TypographyPropsVariantOverrides {}
 }
 
 function App() {
@@ -37,20 +22,7 @@ function App() {
         main: "#00c7f4",
       },
     },
-    typography: {
-      nn42032: {
-        font: "normal normal 400 14px/24px Plus Jakarta Sans",
-      },
-      nn62032: {
-        font: "normal normal 600 12px/16px Plus Jakarta Sans",
-      },
-      nn72032: {
-        font: "normal normal 700 14px/20px Plus Jakarta Sans",
-      },
-      nn74048: {
-        font: "normal normal 700 1.5rem/2rem Plus Jakarta Sans",
-      },
-    },
+    typography: {},
   });
 
   return (
